a302762b933fa1b62d4b6553ff70d5620ec9b74d
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-07-05 20:03:40 -0400

    update: script js
    add: IIFE to add a noscript tag at loading
    add: getLoginData()
    add: getUsersData()
    add: setLoginData()
    add: setUsersData()
    add: setCurrentUser()
    add: getCurrentUser()
    add: logout()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,107 @@
+/**
+ * Redirect
+ * @param {*} url 
+ * @description
+ * Function to redirect to another page
+ */
 function redirect(url) {
     location.href = url;
 }
+
+/**
+ * Get Login Data
+ * @returns {Array<>}
+ * @description
+ * Function to get the login data from Local Storage
+ */
+function getLoginData() {
+    let login = localStorage.getItem('login');
+    login ? login = JSON.parse(login) : login = [];
+
+    return login;
+}
+
+/**
+ * Get Users Data
+ * @returns {Array<>}
+ * @description
+ * Function to get the users data from Local Storage
+ */
+function getUsersData() {
+    let users = localStorage.getItem('users');
+    users ? users = JSON.parse(users) : users = [];
+
+    return users;
+}
+
+/**
+ * Set Login Data
+ * @param {*} data 
+ * @description
+ * Function to set the login data to Local Storage
+ */
+function setLoginData(data) {
+    localStorage.setItem('login', JSON.stringify(data));
+}
+
+/**
+ * Set Login Data
+ * @param {*} data 
+ * @description
+ * Function to set the login data to Local Storage
+ */
+function setUsersData(data) {
+    localStorage.setItem('users', JSON.stringify(data));
+}
+
+/**
+ * Set Current user
+ * @param {*} data 
+ * @description
+ * Function to set the current logged in user id
+ */
+function setCurrentUser(id) {
+    localStorage.setItem('currentUser', id);
+}
+
+/**
+ * Get Current User
+ * @returns {*}
+ * @description
+ * Function to get the current logged in user
+ */
+function getCurrentUser() {
+    const currentUser = localStorage.getItem('currentUser');
+    const users = getUsersData();
+
+    console.log(currentUser);
+    console.log(users);
+    
+    const user = users.find((user) => (user.id == currentUser))
+
+    console.log('User: ', user)
+    return user;
+}
+
+/**
+ * Log Out
+ * @description
+ * Function to log out the current user
+ */
+function logout() {
+    localStorage.removeItem('currentUser');
+    redirect('../index.html');
+}
+
+// Event Listeners
+const logoutEl = document.getElementById('logout');
+if (logoutEl) {
+    logoutEl.addEventListener('click', logout);
+}
+
+(() => {
+    const body = document.body;
+    const noscript = document.createElement('noscript');
+    noscript.textContent = 'Your browser does not support JavaScript!';
+    body.appendChild(noscript);
+})();
