f79526a01000d99c0ff2e5a8938ba3947822b9cd
Jarvis young <youngjarvis24@gmail.com>
2024-07-09 18:46:00 -0400

    webform

---
+++ assets/js/webform.js
@@ -0,0 +1,9 @@
+document.getElementById('workoutform').addEventListener('submit', function ()
+{Event}.preventDefault();
+
+let isValid = true;
+
+// Validate workout type
+const workoutType = document.getElementById('workoutType').vaule;
+const workoutTypeError = document. getElementById('workoutTypeError');
+if (work)
