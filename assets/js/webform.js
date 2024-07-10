10975886c74538f2d244e1ba83d94aeaf4f3c8c0
Jarvis young <youngjarvis24@gmail.com>
2024-07-09 21:49:31 -0400

    worked on formatting

--- assets/js/webform.js
+++ assets/js/webform.js
@@ -1,48 +1,57 @@
-document.getElementById('workoutform').addEventListener('submit', function (Event)
-{Event}.preventDefault();
+document.getElementById('workoutform').addEventListener('submit', function (event) { 
+    event.preventDefault();
+});
 
 let isValid = true;
 
 // Validate workout type
-const WorkoutType = document.getElementById('workoutType').vaule;
-const WorkoutTypeError = document. getElementById('workoutTypeError');
-   if (workoutType.vaule ==="") {}
+const workoutType = document.getElementById('workoutType');
+const workoutTypeError = document.getElementById('workoutTypeError');
+if (workoutType.vaule === "") {
     workoutTypeError.textContent = "select workout type"
     workoutTypeError.style.display = "please select work out";
     localStorage.setItem("workoutType")
     document.getElementById("workoutType").innerHTML = sessionStorage.getItem("workoutType");
-    isValid = false; 
-  } else {
+    isValid = false;
+} else {
     workoutTypeError.style.display = "none";
 }
 
-    
-    
-   
-    // / Validate Duration
-const Duration  = document.getElementById('duration').vaule;
-const DurationError = document. getElementById('durationError');
-   if (duration.vaule ==="" || duration.vaule < 1 || duration.vaule > 120) 
+// / Validate Duration
+const duration = document.getElementById('duration');
+const durationError = document.getElementById('durationError');
+if (duration.vaule === "" || duration.vaule < 1 || duration.vaule > 120) {
     durationError.textContent = "enter duration between 1 and 120"
     durationError.style.display = "please set duration";
-   localStorage.setItem("Duration")
-   document.getElementById("Duration").innerHTML = sessionStorage.getItem("Duration");
-   isValid = false;
-  } else {
+    localStorage.setItem("duration")
+    document.getElementById("duration").innerHTML = sessionStorage.getItem("Duration");
+    isValid = false;
+} else {
     workoutTypeError.style.display = "none";
 }
 
-
-
-    // / Validate Frequncey
-const Frequncey  = document.getElementById('Frequency').vaule;
-const FrequnceyError= document. getElementById('Frequency');
-    if (Frequncey.vaule === "" || Frequncey.vaule <1 || Frequncey.vaule >7)
+// / Validate Frequncey
+const frequncey = document.getElementById('frequency');
+const frequnceyError = document.getElementById('frequency');
+if (frequncey.vaule === "" || frequncey.vaule < 1 || frequncey.vaule > 7) {
     workoutTypeError.textContent = "enter valid ferquency"
     workoutTypeError.style.display = "block";
-    localStorage.setItem("Frequncey")
-    document.getElementById("Frequncey").innerHTML = sessionStorage.getItem("Frequncey");
-    isValid = false; 
+    localStorage.setItem("frequncey")
+    document.getElementById("frequncey").innerHTML = sessionStorage.getItem("Frequncey");
+    isValid = false;
+} else {
+    workoutTypeError.style.display = "none";
+}
+
+// other
+const custom = document.getElementById('custom');
+const customError = document.getElementById('cutomError');
+if (custom.vaule === "") {
+    customError.textContent = "custom option"
+    durationError.style.display = "please input valid command";
+    localStorage.setItem("Duration")
+    document.getElementById("Duration").innerHTML = sessionStorage.getItem("Duration");
+    isValidvalid = false;
 } else {
     workoutTypeError.style.display = "none";
 }
@@ -51,34 +60,18 @@ const FrequnceyError= document. getElementById('Frequency');
 
 
 
-      // other
-    const  Custom = document.getElementById('custom').vaule;
-    const custom = document. getElementById('cutomError');
-    if ( custom.vaule ==="") {}
-    customError.textContent = "Custom option"
-    durationError.style.display = "please input valid command";
-   localStorage.setItem("Duration")
-   document.getElementById("Duration").innerHTML = sessionStorage.getItem("Duration");
-    isValidvalid = false;
-  } else workoutTypeError.style.display = "none";orkoutTypeError.style.display = "none";
-} 
-
-    
+if (isValid) {
+    const data = {
+        workoutType,
+        duration,
+        frequncey,
+        notes: document.getElementById('notes').vaule
 
 
 
-        if (isValid) {
-            const data = {
-                workoutType,
-                duration,
-                frequncey,
-                notes: document. getElementById ('notes').vaule
 
-                
+    };
 
 
-        };
-    
 
-    
-    }
+}
