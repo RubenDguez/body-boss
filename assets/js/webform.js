08cf675992ff42b0a771f57dc358acf38e2c792e
Jarvis young <youngjarvis24@gmail.com>
2024-07-11 19:01:11 -0400

    message webform

--- assets/js/webform.js
+++ assets/js/webform.js
@@ -1,13 +1,11 @@
-document.getElementById('workoutform').addEventListener('submit', function (event) { 
-    event.preventDefault();
-});
+
 
 let isValid = true;
 
 // Validate workout type
 const workoutType = document.getElementById('workoutType');
 const workoutTypeError = document.getElementById('workoutTypeError');
-if (workoutType.vaule === "") {
+if (workoutType.value === "") {
     workoutTypeError.textContent = "select workout type"
     workoutTypeError.style.display = "please select work out";
     localStorage.setItem("workoutType")
@@ -20,10 +18,9 @@ if (workoutType.vaule === "") {
 // / Validate Duration
 const duration = document.getElementById('duration');
 const durationError = document.getElementById('durationError');
-if (duration.vaule === "" || duration.vaule < 1 || duration.vaule > 120) {
+if (duration.value === "" || duration.vaule < 1 || duration.vaule > 120) {
     durationError.textContent = "enter duration between 1 and 120"
     durationError.style.display = "please set duration";
-    localStorage.setItem("duration")
     document.getElementById("duration").innerHTML = sessionStorage.getItem("Duration");
     isValid = false;
 } else {
@@ -33,28 +30,23 @@ if (duration.vaule === "" || duration.vaule < 1 || duration.vaule > 120) {
 // / Validate Frequncey
 const frequncey = document.getElementById('frequency');
 const frequnceyError = document.getElementById('frequency');
-if (frequncey.vaule === "" || frequncey.vaule < 1 || frequncey.vaule > 7) {
-    workoutTypeError.textContent = "enter valid ferquency"
-    workoutTypeError.style.display = "block";
-    localStorage.setItem("frequncey")
-    document.getElementById("frequncey").innerHTML = sessionStorage.getItem("Frequncey");
-    isValid = false;
-} else {
-    workoutTypeError.style.display = "none";
-}
+// if (frequncey.value === "" || frequncey.vaule < 1 || frequncey.vaule > 7) {
+//     workoutTypeError.textContent = "enter valid ferquency"
+//     workoutTypeError.style.display = "block";
+//     localStorage.setItem("frequncey")
+//     document.getElementById("frequncey").innerHTML = sessionStorage.getItem("Frequncey");
+//     isValid = false;
+// } else {
+//     workoutTypeError.style.display = "none";
+// }
 
-// other
-const custom = document.getElementById('custom');
-const customError = document.getElementById('cutomError');
-if (custom.vaule === "") {
-    customError.textContent = "custom option"
-    durationError.style.display = "please input valid command";
-    localStorage.setItem("Duration")
-    document.getElementById("Duration").innerHTML = sessionStorage.getItem("Duration");
-    isValidvalid = false;
-} else {
-    workoutTypeError.style.display = "none";
-}
+
+
+
+
+
+// }
+
 
 
 
@@ -65,13 +57,29 @@ if (isValid) {
         workoutType,
         duration,
         frequncey,
-        notes: document.getElementById('notes').vaule
-
+        notes: document.getElementById('notes').value
 
 
 
     };
 
 
+ 
+    
 
 }
+function submitWorkout(event){
+    event.preventDefault()
+    const newWorkout={
+        workoutType:workoutType.value,
+        duration: duration.value,
+        frequncey: frequncey.value,
+    
+    }
+
+    localStorage.setItem("newWorkout",JSON.stringify(newWorkout))
+
+}
+
+const submitForm = document.getElementById("submit")
+submitForm.addEventListener("submit",submitWorkout)
